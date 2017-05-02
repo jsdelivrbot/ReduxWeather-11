1844dbee15f6fcce8e1910eb6a6cd6456985f591
fergmac <f.macconnell@gmail.com>
2017-05-02 08:24:45 -0700

    create fetchWeather action creator and weather API request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '54341acaf6c1eadc41c4209f9503f137';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// we do this to keep action types consistent between action creators and reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
